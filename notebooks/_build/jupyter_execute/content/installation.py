e4f4fe2adbef8abdba57be4d927e79122c2244c3
Jin Cheong <jcheong0428@gmail.com>
2021-03-03 18:09:01 -0500

    add warning to installation

--- notebooks/_build/jupyter_execute/content/installation.py
+++ notebooks/_build/jupyter_execute/content/installation.py
@@ -2,7 +2,7 @@
 
 *Written by Jin Hyun Cheong*
 
-Open the current notebook in Google Colab and run the cell below to install Py-Feat. Make sure to `Restart Runtime` so that the installation is recognized. 
+Open the current notebook in [Google Colab](http://colab.research.google.com/) and run the cell below to install Py-Feat. Make sure to `Restart Runtime` so that the installation is recognized. 
 
 # Install Py-Feat from source.
 !git clone https://github.com/cosanlab/feat.git  
@@ -11,7 +11,11 @@ Open the current notebook in Google Colab and run the cell below to install Py-F
 !cd feat && python bin/download_models.py
 # Click Runtime from top menu and Restart Runtime! 
 
-Make sure you Restart Runtime before this next step.
+:::{warning}
+Make sure you `Restart Runtime` from the `Runtime` top menu bar to refresh your session. 
+In Kaggle, click `Restart & clear outputs` from the `Run` top menu bar. NOT `Restart session`
+:::
+
 
 # Check Fex class installation
 from feat import Fex
